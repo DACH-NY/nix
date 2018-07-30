28e9c7db4aeaeddd75822de0494340c209bfccdf
Mihaly Elekes <mihaly.elekes@digitalasset.com>
2018-07-30 08:28:11 +0000

    DA: allowing /nix on nfs for ci builds
    Nix tries to remove those extended attributes but this fails if /nix is
    on nfs mount

--- src/libstore/local-store.cc
+++ src/libstore/local-store.cc
@@ -462,6 +462,7 @@ static void canonicalisePathMetaData_(const Path & path, uid_t fromUid, InodesSe
             /* Ignore SELinux security labels since these cannot be
                removed even by root. */
             if (eaName == "security.selinux") continue;
+            if (eaName == "security.selinux" || eaName == "system.nfs4_acl") continue;
             if (lremovexattr(path.c_str(), eaName.c_str()) == -1)
                 throw SysError("removing extended attribute '%s' from '%s'", eaName, path);
         }
